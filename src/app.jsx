e4dd35b040b3ed6d6f1bc9f37040ee09fa433015
erkronell <cedielle@gmail.com>
2021-01-28 08:08:56 -0500

    master

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,21 @@
+import React, { useEffect, useState } from 'react';
 import './app.css';
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(()=>{
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAI1C-Ju8T7LmVZ_swo0tcyTALd-WmPGNQ", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
+
   return (
     <h1>Hello World</h1>
   );
