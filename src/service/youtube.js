f42a076906de4b5cd0e466d08035ed1822dbdae0
Joon <cedielle@gmail.com>
2021-01-29 16:22:27 -0500

    back to the list when searching

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -2,8 +2,8 @@ class Youtube {
   constructor(key) {
     this.key = key;
     this.getRequestOptions = {
-      method: "GET",
-      redirect: "follow",
+      method: 'GET',
+      redirect: 'follow',
     };
   }
 
