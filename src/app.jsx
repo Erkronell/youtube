9da9672227872ca0597f23d06dba025e7b7901e3
Joon <cedielle@gmail.com>
2021-01-29 11:54:48 -0500

    test

--- src/app.jsx
+++ src/app.jsx
@@ -3,41 +3,16 @@ import styles from "./app.module.css";
 import SearchHeader from "./components/search_header/search_header";
 import VideoList from "./components/video_list/video_list";
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
-
   const search = (query) => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${process.env.REACT_APP_API_KEY}`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) =>
-        result.items.map((item) => ({ ...item, id: item.id.videoId }))
-      )
-      .then((items) => setVideos(items))
-      .catch((error) => console.log("error", error));
+    youtube.search(query).then(console.log);
   };
 
   useEffect(() => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=26&key=${process.env.REACT_APP_API_KEY}`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log("error", error));
+    youtube.mostPopular().then(console.log);
   }, []);
+
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search} />
@@ -47,5 +22,3 @@ function App() {
 }
 
 export default App;
-
-// testdsafasdfasdf
---
+++ src/service/youtube.js
@@ -0,0 +1,31 @@
+class Youtube {
+  constructor(key) {
+    this.key = key;
+    this.getRequestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+  }
+
+  mostPopular() {
+    return fetch(
+      `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=26&key=${process.env.REACT_APP_API_KEY}`,
+      this.getRequestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => result.items);
+  }
+
+  search(query) {
+    return fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${process.env.REACT_APP_API_KEY}`,
+      this.getRequestOptions
+    )
+      .then((response) => response.json())
+      .then((result) =>
+        result.items.map((item) => ({ ...item, id: item.id.videoId }))
+      );
+  }
+}
+
+export default Youtube;
