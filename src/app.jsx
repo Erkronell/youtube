3154ddbba65cf1f6bb3df8a05629e05ba0d1fcb5
Joon <cedielle@gmail.com>
2021-01-29 07:44:50 -0500

    search box

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,28 @@
 import React, { useEffect, useState } from "react";
-import "./app.css";
+import styles from "./app.module.css";
+import SearchHeader from "./components/search_header/search_header";
 import VideoList from "./components/video_list/video_list";
 
 function App() {
   const [videos, setVideos] = useState([]);
+
+  const search = query => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${process.env.REACT_APP_API_KEY}`,
+      requestOptions
+    )
+      .then(response => response.json())
+      .then(result =>
+        result.items.map(item => ({ ...item, id: item.id.videoId }))
+      )
+      .then(items => setVideos(items))
+      .catch(error => console.log("error", error));
+  };
 
   useEffect(() => {
     const requestOptions = {
@@ -12,15 +31,19 @@ function App() {
     };
 
     fetch(
-      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAI1C-Ju8T7LmVZ_swo0tcyTALd-WmPGNQ",
+      `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=26&key=${process.env.REACT_APP_API_KEY}`,
       requestOptions
     )
       .then(response => response.json())
       .then(result => setVideos(result.items))
       .catch(error => console.log("error", error));
   }, []);
-
-  return <VideoList videos={videos} />;
+  return (
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
+      <VideoList videos={videos} />
+    </div>
+  );
 }
 
 export default App;
